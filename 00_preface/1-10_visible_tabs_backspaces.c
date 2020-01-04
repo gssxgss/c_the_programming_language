42c8a08292a028a3a7dd2c0dec5116fa217afec4
GSSxGSS <gsshcl@gmail.com>
2020-01-04 18:03:39 +0900

    1-10

---
+++ 00_preface/1-10_visible_tabs_backspaces.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+/**
+ * Exercise 1-10 Make tab backspace and backslash visible
+ * In MacOS, type ctrl+v, ctrl+h to perform a backspace
+ */
+int main () {
+  int c;
+
+  while( (c = getchar()) != EOF) {
+    switch (c) {
+      case '\t':
+        putchar('\\');
+        putchar('t');
+        break;
+      case '\b':
+        putchar('\\');
+        putchar('b');
+        break;
+      case '\\':
+        putchar('\\');
+        putchar('\\');
+        break;
+      default:
+        putchar(c);
+    }
+  }
+  return 0;
+}
