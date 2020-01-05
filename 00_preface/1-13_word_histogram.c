a10fe936abe5474a1a9caca453590da5fe107742
GSSxGSS <gsshcl@gmail.com>
2020-01-05 22:09:07 +0900

    1-13

---
+++ 00_preface/1-13_word_histogram.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+# include <stdbool.h>
+
+/**
+ * Exercise 1-13 Print a word per line
+ */
+int main() {
+  int c;
+  bool is_in_newline = false;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\t' || c == '\n') {
+      if (!is_in_newline) {
+        putchar('\n');
+        is_in_newline = true;
+      }
+    } else {
+      putchar('*');
+      is_in_newline = false;
+    }
+  }
+  return 0;
+}
+  
